a35ceab5d6043b4238db0d916bb814b312b4cd81
damienbod <damien_bod@hotmail.com>
2020-05-29 11:41:14 +0200

    User the API protected my Azure AD

--- MyServerRenderedPortal/Pages/CallApi.cshtml.cs
+++ MyServerRenderedPortal/Pages/CallApi.cshtml.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
 
 namespace MyServerRenderedPortal.Pages
 {
     public class CallApiModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly ApiService _apiService;
 
+        public CallApiModel(ApiService apiService)
+        {
+            _apiService = apiService;
+        }
+
+        public async Task OnGetAsync()
+        {
+            var data = await _apiService.GetApiDataAsync();
         }
     }
 }
