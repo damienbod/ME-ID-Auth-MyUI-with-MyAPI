a9ca5306b6a13cd88f7211b8a2c9d5b44412cc0e
damienbod <damien_bod@hotmail.com>
2020-06-16 16:07:59 +0200

    creating API

--- CreateAppRegistrations/CreateAzureAppRegistration.ps1
+++ CreateAppRegistrations/CreateAzureAppRegistration.ps1
@@ -1,5 +1,8 @@
 $tenantId = "7ff95b15-dc21-4ba6-bc92-824856578fc1"
+$identifier = New-Guid
+$identifierUrl = "api://" + $identifier
 
+Write-Host "created identifierUrl: $identifierUrl"
 Write-Host (az version)
 Write-Host "Azure Create App Registration"
 
@@ -8,13 +11,19 @@ Write-Host "Azure Create App Registration"
 # $data = ($bodyJSON | ConvertFrom-Json).value
 # Write-Host "data: $bodyJSON"
 
-
-$appId = "d5822f38-922b-4a1b-80dd-3a1befa52cae"
+$myApiAppRegistration = az ad app create `
+	--display-name "mi-api" `
+	--available-to-other-tenants true `
+	--oauth2-allow-implicit-flow  false `
+	--identifier-uris $identifierUrl `
+	--required-resource-accesses `@api_required_resources.json 
+	
+$appId = ($myApiAppRegistration | ConvertFrom-Json).appId
 Write-Host "created appId: $appId"
-# az ad app show --id "98328d53-55ec-4f14-8407-0ca5ff2f2d20"
-
-az ad app update --id $appId --set groupMembershipClaims=None
 
 az ad app update --id  $appId --optional-claims `@api_optional_claims.json
+
+# az ad app update --id $appId --set groupMembershipClaims="None"
+# az ad app update --id $appId --set signInAudience="AzureADandPersonalMicrosoftAccount"
 
 az ad app show --id $appId
