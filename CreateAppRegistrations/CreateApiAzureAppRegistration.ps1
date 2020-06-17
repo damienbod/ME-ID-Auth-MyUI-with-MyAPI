a579acf8e7e70c2f47c2da768f478ee9722cbfe9
damienbod <damien_bod@hotmail.com>
2020-06-17 08:14:43 +0200

    clean up code

--- CreateAppRegistrations/CreateApiAzureAppRegistration.ps1
+++ CreateAppRegistrations/CreateApiAzureAppRegistration.ps1
@@ -13,10 +13,12 @@ $userAccessScope = '{
 		"value": "access_as_user"
 }' | ConvertTo-Json | ConvertFrom-Json
 
-
-Write-Host "created identifierUrl: $identifierUrl"
 # Write-Host (az version)
-Write-Host "Azure Create API App Registration"
+Write-Host "Begin API Azure App Registration"
+
+##################################
+### Create Azure App Registration
+##################################
 
 $identifier = New-Guid
 $identifierUrl = "api://" + $identifier 
@@ -29,45 +31,47 @@ $myApiAppRegistration = az ad app create `
 
 $data = ($myApiAppRegistration | ConvertFrom-Json)
 $appId = $data.appId
+Write-Host "Created API $displayName with appId: $appId"
 
-Write-Host "created API $displayName with appId: $appId"
+##################################
+### Add optional claims to App Registration 
+##################################
 
 az ad app update --id $appId --optional-claims `@api_optional_claims.json
+Write-Host "Optional claims added to App Registration: $appId"
 
-###  Add scopes
+##################################
+###  Add scopes (oauth2Permissions)
+##################################
+
 # 1. read oauth2Permissions
 $apiApp = az ad app show --id $appId | Out-String | ConvertFrom-Json
 $oauth2Permissions = $apiApp.oauth2Permissions
 
-# 2. set to enabled to false
+# 2. set to enabled to false from the defualt scope, because we want to remove this
 $oauth2Permissions[0].isEnabled = 'false'
 $oauth2Permissions = ConvertTo-Json -InputObject @($oauth2Permissions) 
-
-#Write-Host "- oauth2Permissions -----------"
-#Write-Host "$oauth2Permissions" 
-
-# 3. disable
+# Write-Host "$oauth2Permissions" 
+# disable oauth2Permission in Azure App Registration
 $oauth2Permissions | Out-File -FilePath .\oauth2Permissionsold.json
 az ad app update --id $appId --set oauth2Permissions=`@oauth2Permissionsold.json
 
-# 4. delete
+# 3. delete the default oauth2Permission
 az ad app update --id $appId --set oauth2Permissions='[]'
 
-# 5. add new scope add the new oauth2Permissions values
+# 4. add the new scope required add the new oauth2Permissions values
 $oauth2PermissionsNew += (ConvertFrom-Json -InputObject $userAccessScope)
 $oauth2PermissionsNew[0].id = $identifier 
 $oauth2PermissionsNew = ConvertTo-Json -InputObject @($oauth2PermissionsNew) 
-
-#Write-Host "- oauth2PermissionsNew --------"
-#Write-Host "$oauth2PermissionsNew" 
-
+# Write-Host "$oauth2PermissionsNew" 
 $oauth2PermissionsNew | Out-File -FilePath .\oauth2Permissionsnew.json
 az ad app update --id $appId --set oauth2Permissions=`@oauth2Permissionsnew.json
 
-# create a ServicePrincipal for the API
-az ad sp create --id $appId
+##################################
+###  Create a ServicePrincipal for the API
+##################################
 
-### Finished adding scopes
-# az ad app show --id $appId
+$sp = az ad sp create --id $appId
+Write-Host "Created Service Principal: $sp"
 
 return $appId
