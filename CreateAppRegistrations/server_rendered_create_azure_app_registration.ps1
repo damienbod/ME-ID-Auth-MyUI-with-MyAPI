d5f4537cf8383bd864a4d792b049c7aa13fc3529
damienbod <damien_bod@hotmail.com>
2020-06-17 12:33:59 +0200

    adding a server rendered app

--- CreateAppRegistrations/server_rendered_create_azure_app_registration.ps1
+++ CreateAppRegistrations/server_rendered_create_azure_app_registration.ps1
@@ -1,32 +1,65 @@
 $tenantId = $args[0]
-$displayName = "mi-api"
-$userAccessScope = '{
-		"lang": null,
-		"origin": "Application",		
-		"adminConsentDescription": "Allow access to the API",
-		"adminConsentDisplayName": "my-api-access",
-		"id": "--- replaced in scripts ---",
-		"isEnabled": true,
-		"type": "User",
-		"userConsentDescription": "Allow access to my-api access_as_user",
-		"userConsentDisplayName": "Allow access to my-api",
-		"value": "access_as_user"
-}' | ConvertTo-Json | ConvertFrom-Json
+$appIdapp = $args[1]
+$displayName = "mi-server-rendered-portal"
+$requiredResourceAccesses = '[
+	{
+		"resourceAppId": "00000003-0000-0000-c000-000000000000",
+		"resourceAccess": [
+			{
+				"id": "64a6cdd6-aab1-4aaf-94b8-3cc8405e90d0",
+				"type": "Scope"
+			},
+			{
+				"id": "7427e0e9-2fba-42fe-b0c0-848c9e6a8182",
+				"type": "Scope"
+			},
+			{
+				"id": "37f7f235-527c-4136-accd-4a02d197296e",
+				"type": "Scope"
+			},
+			{
+				"id": "14dad69e-099b-42c9-810b-d002981feec1",
+				"type": "Scope"
+			},
+			{
+				"id": "e1fe6dd8-ba31-4d61-89e7-88639da4683d",
+				"type": "Scope"
+			}
+		]
+	},
+	{
+		"resourceAppId": "-- replace --",
+		"resourceAccess": [
+			{
+				"id": "-- replace --",
+				"type": "Scope"
+			}
+		]
+	}
+]' | ConvertTo-Json | ConvertFrom-Json
 
 Write-Host "Begin ServerRendered Azure App Registration"
+
+# add the API values to the data
+$requiredResourceAccesses[1].resourceAppId = $identifier 
+$requiredResourceAccesses[1].resourceAccess[0].id = $identifier 
+$requiredResourceAccessesNew = ConvertTo-Json -InputObject @($requiredResourceAccesses) 
+Write-Host "$requiredResourceAccessesNew" 
+$requiredResourceAccessesNew | Out-File -FilePath .\server_rendered_required_resources.json
+Write-Host " - Updated required-resource-accesses  for new App Registration"
 
 ##################################
 ### Create Azure App Registration
 ##################################
 
-$identifier = New-Guid
-$identifierUrl = "api://" + $identifier 
+#$identifier = New-Guid
+#$identifierUrl = "api://" + $identifier 
+
 $myApiAppRegistration = az ad app create `
 	--display-name $displayName `
 	--available-to-other-tenants true `
 	--oauth2-allow-implicit-flow  false `
-	--identifier-uris $identifierUrl `
-	--required-resource-accesses `@api_required_resources.json 
+	--required-resource-accesses `@server_rendered_required_resources.json
 
 $data = ($myApiAppRegistration | ConvertFrom-Json)
 $appId = $data.appId
@@ -38,34 +71,6 @@ Write-Host " - Created ServerRendered $displayName with appId: $appId"
 
 az ad app update --id $appId --optional-claims `@api_optional_claims.json
 Write-Host " - Optional claims added to App Registration: $appId"
-
-##################################
-###  Add scopes (oauth2Permissions)
-##################################
-
-# 1. read oauth2Permissions
-$apiApp = az ad app show --id $appId | Out-String | ConvertFrom-Json
-$oauth2Permissions = $apiApp.oauth2Permissions
-
-# 2. set to enabled to false from the defualt scope, because we want to remove this
-$oauth2Permissions[0].isEnabled = 'false'
-$oauth2Permissions = ConvertTo-Json -InputObject @($oauth2Permissions) 
-# Write-Host "$oauth2Permissions" 
-# disable oauth2Permission in Azure App Registration
-$oauth2Permissions | Out-File -FilePath .\oauth2Permissionsold.json
-az ad app update --id $appId --set oauth2Permissions=`@oauth2Permissionsold.json
-
-# 3. delete the default oauth2Permission
-az ad app update --id $appId --set oauth2Permissions='[]'
-
-# 4. add the new scope required add the new oauth2Permissions values
-$oauth2PermissionsNew += (ConvertFrom-Json -InputObject $userAccessScope)
-$oauth2PermissionsNew[0].id = $identifier 
-$oauth2PermissionsNew = ConvertTo-Json -InputObject @($oauth2PermissionsNew) 
-# Write-Host "$oauth2PermissionsNew" 
-$oauth2PermissionsNew | Out-File -FilePath .\oauth2Permissionsnew.json
-az ad app update --id $appId --set oauth2Permissions=`@oauth2Permissionsnew.json
-Write-Host " - Updated scopes (oauth2Permissions) for App Registration: $appId"
 
 ##################################
 ###  Create a ServicePrincipal for the ServerRendered App Registration
