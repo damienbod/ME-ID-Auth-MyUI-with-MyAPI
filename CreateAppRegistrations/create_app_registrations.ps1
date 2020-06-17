63398b331fdf3625856533e6341b3687fa33c7da
damienbod <damien_bod@hotmail.com>
2020-06-17 12:08:03 +0200

    updating scripts

--- CreateAppRegistrations/create_app_registrations.ps1
+++ CreateAppRegistrations/create_app_registrations.ps1
@@ -2,5 +2,5 @@ $tenantId = "7ff95b15-dc21-4ba6-bc92-824856578fc1"
 
 # Write-Host (az version)
 
-$appIdApi = &".\CreateApiAzureAppRegistration.ps1" $tenantId | select -Last 1
+$appIdApi = &".\api_create_azure_app_registration.ps1" $tenantId | select -Last 1
 Write-Host "Created Api App registraion: $appIdApi"
