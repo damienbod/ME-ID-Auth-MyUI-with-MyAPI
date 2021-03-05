21e0f66333a0447faf0c37323859469b3958f5c6
damienbod <damien_bod@hotmail.com>
2021-03-05 11:28:13 +0100

    Update packages and switching to refresh tokens due to iframe problems

--- MySpaAngularUI/src/app/app.module.ts
+++ MySpaAngularUI/src/app/app.module.ts
@@ -15,13 +15,14 @@ export function configureAuth(oidcConfigService: OidcConfigService) {
             authWellknownEndpoint: 'https://login.microsoftonline.com/7ff95b15-dc21-4ba6-bc92-824856578fc1/v2.0',
             redirectUrl: window.location.origin,
             clientId: 'ad6b0351-92b4-4ee9-ac8d-3e76e5fd1c67',
-            scope: 'openid profile email api://98328d53-55ec-4f14-8407-0ca5ff2f2d20/access_as_user',
+            scope: 'openid profile offline_access email api://98328d53-55ec-4f14-8407-0ca5ff2f2d20/access_as_user',
             responseType: 'code',
             silentRenew: true,
+            useRefreshToken: true,
+            ignoreNonceAfterRefresh: true,
             maxIdTokenIatOffsetAllowedInSeconds: 600,
             issValidationOff: false, // this needs to be true if using a common endpoint in Azure
             autoUserinfo: false,
-            silentRenewUrl: window.location.origin + '/silent-renew.html',
             logLevel: LogLevel.Debug
     });
 }
