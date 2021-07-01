eb9c4474bea368f3650ae3b801d8f4253fb1875b
damienbod <damien_bod@hotmail.com>
2021-07-01 09:10:44 +0200

    Updating config settngs

--- MySpaAngularUI/src/app/app.module.ts
+++ MySpaAngularUI/src/app/app.module.ts
@@ -1,34 +1,14 @@
-import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-import { AuthModule, OidcConfigService, LogLevel } from 'angular-auth-oidc-client';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { UnauthorizedComponent } from './unauthorized/unauthorized.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptor } from './auth.interceptor';
 
-export function configureAuth(oidcConfigService: OidcConfigService) {
-  return () =>
-    oidcConfigService.withConfig({
-            stsServer: 'https://login.microsoftonline.com/7ff95b15-dc21-4ba6-bc92-824856578fc1/v2.0',
-            authWellknownEndpoint: 'https://login.microsoftonline.com/7ff95b15-dc21-4ba6-bc92-824856578fc1/v2.0',
-            redirectUrl: window.location.origin,
-            clientId: 'ad6b0351-92b4-4ee9-ac8d-3e76e5fd1c67',
-            scope: 'openid profile offline_access email api://98328d53-55ec-4f14-8407-0ca5ff2f2d20/access_as_user',
-            responseType: 'code',
-            silentRenew: true,
-            useRefreshToken: true,
-            ignoreNonceAfterRefresh: true,
-            maxIdTokenIatOffsetAllowedInSeconds: 600,
-            issValidationOff: false, // this needs to be true if using a common endpoint in Azure
-            autoUserinfo: false,
-            logLevel: LogLevel.Debug,
-            customParams: {
-              prompt: 'select_account', // login, consent
-            },
-    });
-}
+import { NgModule } from '@angular/core';
+import { AuthModule, LogLevel } from 'angular-auth-oidc-client';
+
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, UnauthorizedComponent],
@@ -38,24 +18,30 @@ export function configureAuth(oidcConfigService: OidcConfigService) {
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'unauthorized', component: UnauthorizedComponent },
-], { relativeLinkResolution: 'legacy' }),
-    AuthModule.forRoot(),
+  ], { relativeLinkResolution: 'legacy' }),
+  AuthModule.forRoot({
+    config: {
+      stsServer: 'https://login.microsoftonline.com/7ff95b15-dc21-4ba6-bc92-824856578fc1/v2.0',
+      authWellknownEndpointUrl: 'https://login.microsoftonline.com/7ff95b15-dc21-4ba6-bc92-824856578fc1/v2.0',
+      redirectUrl: window.location.origin,
+      clientId: 'ad6b0351-92b4-4ee9-ac8d-3e76e5fd1c67',
+      scope: 'openid profile offline_access email api://98328d53-55ec-4f14-8407-0ca5ff2f2d20/access_as_user',
+      responseType: 'code',
+      silentRenew: true,
+      useRefreshToken: true,
+      ignoreNonceAfterRefresh: true,
+      maxIdTokenIatOffsetAllowedInSeconds: 600,
+      issValidationOff: false, // this needs to be true if using a common endpoint in Azure
+      autoUserInfo: false,
+      logLevel: LogLevel.Debug,
+      customParamsAuthRequest: {
+        prompt: 'select_account', // login, consent
+      },
+    },
+  }),
     HttpClientModule,
   ],
-  providers: [
-    OidcConfigService,
-    {
-      provide: APP_INITIALIZER,
-      useFactory: configureAuth,
-      deps: [OidcConfigService],
-      multi: true,
-    },
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptor,
-      multi: true,
-    },
-  ],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
