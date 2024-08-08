a98280e86dc404a76db247f187adbfb1ba20669f
damienbod <damien_bod@hotmail.com>
2024-08-08 21:47:00 +0200

    angular 18

--- MySpaAngularUI/src/app/app.module.ts
+++ MySpaAngularUI/src/app/app.module.ts
@@ -3,51 +3,45 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { UnauthorizedComponent } from './unauthorized/unauthorized.component';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { AuthInterceptor } from './auth.interceptor';
 
 import { NgModule } from '@angular/core';
 import { AuthModule, LogLevel } from 'angular-auth-oidc-client';
 
 
-@NgModule({
-  declarations: [AppComponent, HomeComponent, UnauthorizedComponent],
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot([
-    { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'home', component: HomeComponent },
-    { path: 'unauthorized', component: UnauthorizedComponent },
-  ], {}),
-  AuthModule.forRoot({
-    config: {
-      authority: 'https://login.microsoftonline.com/7ff95b15-dc21-4ba6-bc92-824856578fc1/v2.0',
-      authWellknownEndpointUrl: 'https://login.microsoftonline.com/7ff95b15-dc21-4ba6-bc92-824856578fc1/v2.0',
-      redirectUrl: window.location.origin,
-      clientId: 'ad6b0351-92b4-4ee9-ac8d-3e76e5fd1c67',
-      scope: 'openid profile offline_access email api://98328d53-55ec-4f14-8407-0ca5ff2f2d20/access_as_user',
-      responseType: 'code',
-      silentRenew: true,
-      useRefreshToken: true,
-      ignoreNonceAfterRefresh: true,
-      maxIdTokenIatOffsetAllowedInSeconds: 600,
-      issValidationOff: false, // this needs to be true if using a common endpoint in Azure
-      autoUserInfo: false,
-      logLevel: LogLevel.Debug,
-      customParamsAuthRequest: {
-        prompt: 'select_account', // login, consent
-      },
-    },
-  }),
-    HttpClientModule,
-  ],
-  providers: [
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptor,
-      multi: true,
-    }
-  ],
-  bootstrap: [AppComponent],
-})
+@NgModule({ declarations: [AppComponent, HomeComponent, UnauthorizedComponent],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        RouterModule.forRoot([
+            { path: '', redirectTo: 'home', pathMatch: 'full' },
+            { path: 'home', component: HomeComponent },
+            { path: 'unauthorized', component: UnauthorizedComponent },
+        ], {}),
+        AuthModule.forRoot({
+            config: {
+                authority: 'https://login.microsoftonline.com/7ff95b15-dc21-4ba6-bc92-824856578fc1/v2.0',
+                authWellknownEndpointUrl: 'https://login.microsoftonline.com/7ff95b15-dc21-4ba6-bc92-824856578fc1/v2.0',
+                redirectUrl: window.location.origin,
+                clientId: 'ad6b0351-92b4-4ee9-ac8d-3e76e5fd1c67',
+                scope: 'openid profile offline_access email api://98328d53-55ec-4f14-8407-0ca5ff2f2d20/access_as_user',
+                responseType: 'code',
+                silentRenew: true,
+                useRefreshToken: true,
+                ignoreNonceAfterRefresh: true,
+                maxIdTokenIatOffsetAllowedInSeconds: 600,
+                issValidationOff: false, // this needs to be true if using a common endpoint in Azure
+                autoUserInfo: false,
+                logLevel: LogLevel.Debug,
+                customParamsAuthRequest: {
+                    prompt: 'select_account', // login, consent
+                },
+            },
+        })], providers: [
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: AuthInterceptor,
+            multi: true,
+        },
+        provideHttpClient(withInterceptorsFromDi())
+    ] })
 export class AppModule {}
