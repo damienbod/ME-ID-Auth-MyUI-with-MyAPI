291353fad8fee104354d256223eb7e2a5e572b67
damienbod <damien_bod@hotmail.com>
2024-10-03 16:29:42 +0200

    short form for same thing

--- MyServerRenderedPortal/SecurityHeadersDefinitions.cs
+++ MyServerRenderedPortal/SecurityHeadersDefinitions.cs
@@ -26,23 +26,7 @@ public static class SecurityHeadersDefinitions
                 builder.AddFrameAncestors().None();
             })
             .RemoveServerHeader()
-            .AddPermissionsPolicy(builder =>
-            {
-                builder.AddAccelerometer().None();
-                builder.AddAutoplay().None();
-                builder.AddCamera().None();
-                builder.AddEncryptedMedia().None();
-                builder.AddFullscreen().All();
-                builder.AddGeolocation().None();
-                builder.AddGyroscope().None();
-                builder.AddMagnetometer().None();
-                builder.AddMicrophone().None();
-                builder.AddMidi().None();
-                builder.AddPayment().None();
-                builder.AddPictureInPicture().None();
-                builder.AddSyncXHR().None();
-                builder.AddUsb().None();
-            });
+            .AddPermissionsPolicyWithDefaultSecureDirectives();
 
         if (!isDev)
         {
